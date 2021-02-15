f944eb72030b2ae158f34c83650f64ed21d88014
JonGudenzi <jongudenzi@gmail.com>
2021-02-15 18:56:31 -0500

    start of cloned repo

--- controllers/beer_controller.js
+++ controllers/beer_controller.js
@@ -2,29 +2,18 @@ var express = require("express");
 var router = express.Router();
 var beer = require("../models/beer.js");
 
-// router.post("/beers/create", async (req, res) => {
-//   beer.create(["brewery_name", "beer_name", "beer_style", "alcohol_content", "beer_taste", "beer_pairing", "ordered"],
-//     [req.body.name, req.body.false], (result) => {
-//       res.json({ result });
-//     });
-// });
-
-
 router.post('/api/beers/create', async (req, res) => {
-  // edited burger create to add in a burger_name
   const newBeer = new db.beers({
     beer_name: req.body.beer_name,
   });
   try {
     const dbBeer = await newBeer.save();
-    // redirect
     res.redirect('/');
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-/////////////////////////////////////////////////////////////////////////
 router.put("/beers/ordered/:id", (req, res) => {
   var condition = "id = " + req.params.id;
 
@@ -37,5 +26,4 @@ router.put("/beers/ordered/:id", (req, res) => {
   console.log("condition", condition);
 });
 
-// Export routes for server.js to use.
 module.exports = router;
--- routes/beer-api-routes.js
+++ routes/beer-api-routes.js
@@ -1,14 +1,6 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-// Dependencies
-// =============================================================
-// Requiring our models
 var db = require("../models");
-// Routes
-// =============================================================
+
 module.exports = function(app) {
-  // GET route for getting all of the beers
   app.get("/api/beer", function(req, res) {
     var query = {};
     console.log(req.query.q);
@@ -16,27 +8,9 @@ module.exports = function(app) {
       query.beer_name = req.query.q;
       res.redirect("/beers/"+req.query.q);
     }
-    // else {
-    //   res.json (true);
-    // }
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
-    // db.beer.findAll({
-    //   where: query,
-
-      
-    // }).then(function(dbbeer) {
-    //   res.json(dbbeer);
-    //   console.log(dbbeer);
-    // });
   });
   
-  // Get route for retrieving a single beer
   app.get("/api/beer/:id", function(req, res) {
-    // Here we add an "include" property to our options in our findOne query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
     db.beer.findOne({
       where: {
         id: req.params.id
@@ -45,13 +19,6 @@ module.exports = function(app) {
       res.json(dbbeer);
     });
   });
-  // beer route for saving a new beer
-  // app.post("/api/beer", function(req, res) {
-  //   db.beer.create(req.body).then(function(dbbeer) {
-  //     res.json(dbbeer);
-  //   });
-  // });
-  // DELETE route for deleting beers
   app.delete("/api/beer/:id", function(req, res) {
     db.beer.destroy({
       where: {
@@ -61,7 +28,7 @@ module.exports = function(app) {
       res.json(dbbeer);
     });
   });
-  // PUT route for updating beers
+ 
   app.put("/api/beer", function(req, res) {
     db.beer.update(
       req.body,
